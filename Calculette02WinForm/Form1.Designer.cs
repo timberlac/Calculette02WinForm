eabbe99b76627cc46944dd443105e28bc48f35f5
Eric <Eric@ACER_WORK_ERIC>
2021-01-17 12:29:12 +0100

    Ajoutez des fichiers projet.

---
+++ Calculette02WinForm/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿
+namespace Calculette02WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnMultiplier = new System.Windows.Forms.Button();
+            this.txtValeur1 = new System.Windows.Forms.TextBox();
+            this.txtValeur2 = new System.Windows.Forms.TextBox();
+            this.txtResultat = new System.Windows.Forms.TextBox();
+            this.lblOperation = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.btnEffacer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddition.Location = new System.Drawing.Point(189, 22);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(56, 51);
+            this.btnAddition.TabIndex = 0;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = true;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnMultiplier
+            // 
+            this.btnMultiplier.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplier.Location = new System.Drawing.Point(188, 96);
+            this.btnMultiplier.Name = "btnMultiplier";
+            this.btnMultiplier.Size = new System.Drawing.Size(56, 49);
+            this.btnMultiplier.TabIndex = 1;
+            this.btnMultiplier.Text = "x";
+            this.btnMultiplier.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.btnMultiplier.UseCompatibleTextRendering = true;
+            this.btnMultiplier.UseVisualStyleBackColor = true;
+            this.btnMultiplier.Click += new System.EventHandler(this.btnMultiplier_Click);
+            // 
+            // txtValeur1
+            // 
+            this.txtValeur1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtValeur1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValeur1.Location = new System.Drawing.Point(59, 14);
+            this.txtValeur1.Name = "txtValeur1";
+            this.txtValeur1.Size = new System.Drawing.Size(104, 35);
+            this.txtValeur1.TabIndex = 4;
+            this.txtValeur1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtValeur1.TextChanged += new System.EventHandler(this.txtValeur1_TextChanged);
+            // 
+            // txtValeur2
+            // 
+            this.txtValeur2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValeur2.Location = new System.Drawing.Point(60, 57);
+            this.txtValeur2.Name = "txtValeur2";
+            this.txtValeur2.Size = new System.Drawing.Size(103, 35);
+            this.txtValeur2.TabIndex = 5;
+            this.txtValeur2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtValeur2.TextChanged += new System.EventHandler(this.txtValeur2_TextChanged);
+            // 
+            // txtResultat
+            // 
+            this.txtResultat.BackColor = System.Drawing.SystemColors.Menu;
+            this.txtResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultat.Location = new System.Drawing.Point(63, 116);
+            this.txtResultat.Name = "txtResultat";
+            this.txtResultat.Size = new System.Drawing.Size(100, 35);
+            this.txtResultat.TabIndex = 6;
+            this.txtResultat.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblOperation
+            // 
+            this.lblOperation.AutoSize = true;
+            this.lblOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperation.Location = new System.Drawing.Point(16, 53);
+            this.lblOperation.Name = "lblOperation";
+            this.lblOperation.Size = new System.Drawing.Size(28, 29);
+            this.lblOperation.TabIndex = 7;
+            this.lblOperation.Text = "+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 29);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "=";
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.BackgroundImage = global::Calculette02WinForm.Properties.Resources.quitter;
+            this.btnQuitter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnQuitter.Location = new System.Drawing.Point(274, 96);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(49, 49);
+            this.btnQuitter.TabIndex = 3;
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // btnEffacer
+            // 
+            this.btnEffacer.BackgroundImage = global::Calculette02WinForm.Properties.Resources.gomme;
+            this.btnEffacer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnEffacer.Location = new System.Drawing.Point(274, 23);
+            this.btnEffacer.Name = "btnEffacer";
+            this.btnEffacer.Size = new System.Drawing.Size(51, 50);
+            this.btnEffacer.TabIndex = 2;
+            this.btnEffacer.UseVisualStyleBackColor = true;
+            this.btnEffacer.Click += new System.EventHandler(this.btnEffacer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 174);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblOperation);
+            this.Controls.Add(this.txtResultat);
+            this.Controls.Add(this.txtValeur2);
+            this.Controls.Add(this.txtValeur1);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.btnEffacer);
+            this.Controls.Add(this.btnMultiplier);
+            this.Controls.Add(this.btnAddition);
+            this.Name = "Form1";
+            this.Text = "Calculette02WF";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.Button btnMultiplier;
+        private System.Windows.Forms.Button btnEffacer;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.TextBox txtValeur1;
+        private System.Windows.Forms.TextBox txtValeur2;
+        private System.Windows.Forms.TextBox txtResultat;
+        private System.Windows.Forms.Label lblOperation;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Calculette02WinForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculette02WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEffacer_Click(object sender, EventArgs e)
+        {
+            txtValeur1.Text = "";
+            txtValeur2.Text = "";
+            txtResultat.Text = "";
+            lblOperation.Text = "";
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            try            // évite les plantage si les valeurs sont fausses
+            {
+                txtResultat.Text = (float.Parse(txtValeur1.Text) + float.Parse(txtValeur2.Text)).ToString();
+                lblOperation.Text = "+";
+            }
+            catch
+            { }
+        }
+
+        private void btnMultiplier_Click(object sender, EventArgs e)
+        {
+            try            // évite les plantage si les valeurs sont fausses
+            {
+                txtResultat.Text = (float.Parse(txtValeur1.Text) * float.Parse(txtValeur2.Text)).ToString();
+                lblOperation.Text = "X";
+            }
+            catch
+            { }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnEffacer_Click(null,null);
+        }
+
+        private void txtValeur1_TextChanged(object sender, EventArgs e)
+        {
+            annuleOperations();
+        }
+
+        private void txtValeur2_TextChanged(object sender, EventArgs e)
+        {
+            annuleOperations();
+        }
+        private void annuleOperations()
+        {
+            txtResultat.Text = "";
+            lblOperation.Text = "";
+        }
+    }
+}
